4a9916944a89d2cf600dab80bd44b89f96b9941c
William Li <githuberr@outlook.com>
2018-04-30 22:58:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,7 @@ def turn(board)
   loop do
     puts "Please enter 1-9:"
     input = gets.chomp
-    index = input.to_i-1
+    index = input_to_index(input)
     if !valid_move?(board, index)
       next
     else
